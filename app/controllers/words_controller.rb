c1220809c094d0693382080b582c067240d02b8f
Omer Perez <omerperez24@gmail.com>
2021-10-18 20:36:52 -0400

    Add new and create methods in controller

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -4,4 +4,22 @@ class WordsController < ApplicationController
     @words = Word.all
   end
 
+  def new
+    @word = Word.new
+  end
+
+  def create
+    @word = Word.new(word_params)
+    if @word.save
+      redirect_to(words_path)
+    else
+      render :new
+    end  
+  end
+
+  private
+
+  def word_params
+    params.require(:word).permit(:value,:language) 
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :words, only: :index do
+  resources :words, only: [:index, :new, :create] do
   end
 end
