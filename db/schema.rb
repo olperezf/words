965a09fbb996afd198d929aab9e58a4978fd3cc8
Omer Perez <omerperez24@gmail.com>
2021-10-20 15:30:11 -0400

    Remove language column from words

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_20_191009) do
+ActiveRecord::Schema.define(version: 2021_10_20_192743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 2021_10_20_191009) do
 
   create_table "words", force: :cascade do |t|
     t.string "content"
-    t.string "language"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
