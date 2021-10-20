b16f3558cdfd80d67cefb8acea257a97bcb5320b
Omer Perez <omerperez24@gmail.com>
2021-10-20 15:24:19 -0400

    Create model Language

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_20_151847) do
+ActiveRecord::Schema.define(version: 2021_10_20_191009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "languages", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "words", force: :cascade do |t|
     t.string "content"
