da210f48dcf0a255236e7fccedcce7b97958ed8b
Omer Perez <omerperez24@gmail.com>
2021-10-20 14:46:58 -0400

    Validate inclusion of language

--- spec/controllers/words_controller_spec.rb
+++ spec/controllers/words_controller_spec.rb
@@ -43,7 +43,7 @@ RSpec.describe WordsController, type: :controller do
     
     context 'valid params' do
       let(:params) do
-        {word: { content: 'cat', language: 'english' } } 
+        {word: { content: 'cat', language: 'English' } } 
       end  
       it 'creates new word' do
         expect { subject }.to change(Word, :count).from(0).to(1)
