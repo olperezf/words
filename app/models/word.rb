4cde8080dd11eec88d8f2e5ee53cc2f5b6cebe86
Omer Perez <omerperez24@gmail.com>
2021-11-01 11:30:45 -0400

    Do not allow translations to be in the same language as word

--- app/models/word.rb
+++ app/models/word.rb
@@ -7,8 +7,18 @@ class Word < ApplicationRecord
   has_many :inverse_translations_association, class_name: 'Translation', foreign_key: 'translated_word_id'
   has_many :inverse_translations, through: :inverse_translations_association, source: :word
   
-  accepts_nested_attributes_for :translations, reject_if: :all_blank, allow_destroy: true 
-  validates :content , :language, presence: true
 
+  validates :content , :language, presence: true
+  validate :translations_cannot_be_in_the_same_language_as_word
+  
+  accepts_nested_attributes_for :translations, reject_if: :all_blank, allow_destroy: true 
+  
   paginates_per 10
+  
+  private
+  
+  def translations_cannot_be_in_the_same_language_as_word
+    return if translations.none? { |translation| translation.language == language } 
+    errors.add(:language, 'must be different than language of translations.') 
+  end
 end 
--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -37,5 +37,34 @@ RSpec.describe Word, type: :model do
   
   describe 'validations' do
     it { is_expected.to validate_presence_of(:content) }
+
+    describe 'translations_cannot_be_in_the_same_language_as_word' do
+      let(:language_1) { FactoryBot.create(:language) }
+      let(:word_1) { FactoryBot.build(:word, language: language_1) }
+
+
+      context 'when translations is in the same language' do
+        let(:word_2) { FactoryBot.build(:word, language: language_1) }
+        before do
+          word_1.translations = [word_2]
+        end
+        it do
+          expect(word_1).to be_invalid
+        end
+      end
+
+      context 'when translations is NOT in the same language' do
+        let(:language_2) { FactoryBot.create(:language, :spanish) }
+        let(:word_2) { FactoryBot.build(:word, language: language_2) }
+        before do
+          word_1.translations = [word_2]
+        end
+        it do
+          expect(word_1).to be_valid
+        end
+      end
+
+    end
+
   end
 end
